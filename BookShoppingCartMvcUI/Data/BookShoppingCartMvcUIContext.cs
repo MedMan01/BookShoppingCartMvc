a28027da2245b12a000ceb0c69491fc63969145a
MedMan01 <148995934+MedMan01@users.noreply.github.com>
2024-01-27 17:35:23 +0100

    Initial commit

---
+++ BookShoppingCartMvcUI/Data/BookShoppingCartMvcUIContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using BookShoppingCartMvcUI.Models;
+
+namespace BookShoppingCartMvcUI.Data
+{
+    public class BookShoppingCartMvcUIContext : DbContext
+    {
+        public BookShoppingCartMvcUIContext (DbContextOptions<BookShoppingCartMvcUIContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<BookShoppingCartMvcUI.Models.Book> Book { get; set; } = default!;
+    }
+}
